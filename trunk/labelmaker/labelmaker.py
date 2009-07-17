880f191ac58cc488af0c2fdc85bfd4506787f3a1
phooky <phooky@89f5bb60-11dc-4128-b5cc-92810caebcc6>
2009-07-17 02:56:39 +0000

    fix for wrong page size
    git-svn-id: http://svn.makerbot.com@290 89f5bb60-11dc-4128-b5cc-92810caebcc6

--- trunk/labelmaker/labelmaker.py
+++ trunk/labelmaker/labelmaker.py
@@ -3,6 +3,7 @@
 from reportlab.pdfgen import canvas
 from reportlab.rl_config import defaultPageSize
 from reportlab.lib.units import inch
+from reportlab.lib.pagesizes import letter
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
 
@@ -10,10 +11,11 @@ LABEL_ROWS = 5
 LABEL_COLUMNS = 2
 PAGE_HEIGHT = defaultPageSize[1]
 PAGE_WIDTH = defaultPageSize[0]
-LABEL_HEIGHT = PAGE_HEIGHT/float(LABEL_ROWS)
-LABEL_WIDTH = PAGE_WIDTH/float(LABEL_COLUMNS)
+LABEL_HEIGHT = 2*inch
+LABEL_WIDTH = 4*inch
 
 pdfmetrics.registerFont(TTFont('DesignerBlock', 'fonts/DESIB___.TTF'))
+pdfmetrics.registerFont(TTFont('Sans', 'fonts/FreeSans.ttf'))
 
 LOGO_WIDTH=2611.0
 LOGO_HEIGHT=212.0
@@ -31,9 +33,9 @@ def drawTitle(c,title,subtitle):
     c.saveState()
     y = LABEL_HEIGHT/2.0
     x = LABEL_WIDTH/2.0
-    c.setFont('DesignerBlock',24)
-    c.drawCentredString(x,y,title)
     c.setFont('DesignerBlock',20)
+    c.drawCentredString(x,y,title)
+    c.setFont('DesignerBlock',18)
     y -= 18
     c.drawCentredString(x,y,subtitle)
     c.restoreState()
@@ -42,17 +44,24 @@ def drawInstructions(c,url):
     c.saveState()
     y = LABEL_HEIGHT/2.0 - 35
     x = LABEL_WIDTH/2.0
+    c.setFont('Sans',16)
     c.drawCentredString(x,y,"Instructions: "+url)
     c.restoreState()
 
-c = canvas.Canvas("hello.pdf")
+c = canvas.Canvas("hello.pdf",pagesize=letter)
+# sticker sheet: 8.25x10
+# page size: 8.5x11
+# translate up and over by .125,.5
+c.translate(0.125*inch,0.5*inch)
 for x in range(LABEL_COLUMNS):
     for y in range(LABEL_ROWS):
         c.saveState()
-        c.translate(x*LABEL_WIDTH,y*LABEL_HEIGHT)
+        if (x != 0):
+            c.translate(LABEL_WIDTH+(0.25*inch),0)
+        c.translate(0,y*LABEL_HEIGHT)
         drawLogo(c)
-        drawTitle(c,"Cupcake CNC","XY lasercut parts")
-        drawInstructions(c,"http://instructions")
+        drawTitle(c,"RepRap Motherboard v1.2","")
+        drawInstructions(c,"make.rrrf.org/rmb-1.2")
         c.restoreState()
 c.showPage()
 c.save()
