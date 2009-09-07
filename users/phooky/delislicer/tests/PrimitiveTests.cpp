67dcd877cd31ac175737a7ebfabf5ac9412d70b5
phooky <phooky@89f5bb60-11dc-4128-b5cc-92810caebcc6>
2009-09-07 04:20:28 +0000

    Was I really writing my own matrix library?  Uh.  Anyway, using eigen2.
    git-svn-id: http://svn.makerbot.com@397 89f5bb60-11dc-4128-b5cc-92810caebcc6

--- users/phooky/delislicer/tests/PrimitiveTests.cpp
+++ users/phooky/delislicer/tests/PrimitiveTests.cpp
@@ -1,41 +1,46 @@
 #include "PrimitiveTests.hpp"
-#include "../Primitives.h"
+#include <Eigen/Core>
+// Eigen/Array is required to support vector::operator==.
+// Strange but true!
+#include <Eigen/Array>
 #include <iostream>
 
 using namespace std;
+USING_PART_OF_NAMESPACE_EIGEN
 
-Point3D makeTestPoint(double x, double y, double z) {
-  Point3D p;
-  p[0] = x; p[1] = y; p[2] = z;
-  return p;
+Vector3d makeTestPoint(double x, double y, double z) {
+  return Vector3d(x,y,z);
 }
 		   
 void PrimitivesTestSuite::constructionTest() {
-  Point3D p = makeTestPoint(1.0,-2.0,3.0);
+  Vector3d p = makeTestPoint(1.0,-2.0,3.0);
   TEST_ASSERT(p[0] == 1.0);
   TEST_ASSERT(p[1] == -2.0);
   TEST_ASSERT(p[2] == 3.0);
 
-  Matrix3D m;
+  Matrix3d m = Matrix3d::Identity();
   for (int i = 0; i < 3; i++) {
     for (int j = 0; j < 3; j++) {
-      TEST_ASSERT(m[i][j] == ((i==j)?1.0:0.0));
+      TEST_ASSERT(m(i,j) == ((i==j)?1.0:0.0));
     }
   }
 
-  Point3D p2 = m.apply(p);
+  Vector3d p2 = m * p;
   TEST_ASSERT(p==p2);
 }
 
 void PrimitivesTestSuite::scalingTest() {
   const double testScale = 2.5;
-  Matrix3D m = Matrix3D::makeScale(testScale);
-  Point3D p = makeTestPoint(1.0,-2.0,3.0);
+  Matrix3d m = Matrix3d::Identity() * testScale;
+  Vector3d p = makeTestPoint(1.0,-2.0,3.0);
   for (int i = 0; i < 3; i++) {
     for (int j = 0; j < 3; j++) {
-      TEST_ASSERT(m[i][j] == ((i==j)?testScale:0.0));
+      TEST_ASSERT(m(i,j) == ((i==j)?testScale:0.0));
     }
   }
-  Point3D p2 = m.apply(p);
+  Vector3d p2 = m * p;
   TEST_ASSERT(p2 == makeTestPoint(2.5,-5.0,7.5));
+}
+
+void PrimitivesTestSuite::rotationTest() {
 }
