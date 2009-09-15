f82baf2abd1c842a1943533f4a30cb012fbd48e7
phooky <phooky@89f5bb60-11dc-4128-b5cc-92810caebcc6>
2009-09-15 18:54:04 +0000

    merged fix from trunk
    git-svn-id: http://svn.makerbot.com@411 89f5bb60-11dc-4128-b5cc-92810caebcc6

--- tags/firmware-1.4/ArduinoSlaveExtruder/RS485.h
+++ tags/firmware-1.4/ArduinoSlaveExtruder/RS485.h
@@ -1,15 +1,14 @@
 void rs485_tx(byte b)
 {
   Serial.print(b, BYTE);
-
+  
   //read for our own byte.
   long now = millis();
   long end = now + 10;
   int tmp = Serial.read();
-  while (tmp != b)
-  {
+  while (tmp != b) {
     tmp = Serial.read();
-	if (millis() > tmp)
-		break;
+    if (millis() > end)
+      break;
   }
 }
