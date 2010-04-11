b722715276321999d76975ca108035974fa1c5eb
hoeken <hoeken@89f5bb60-11dc-4128-b5cc-92810caebcc6>
2010-04-11 22:05:06 +0000

    added parabola design.
    git-svn-id: http://svn.makerbot.com@834 89f5bb60-11dc-4128-b5cc-92810caebcc6

---
+++ users/hoeken/parabola/parabola.py
@@ -0,0 +1,36 @@
+import sdxf
+
+# Set your parabola's parameters here
+focus=60
+range=(-140.0,140.0)
+delta=5
+centerRadius = 0.5  # radius of circle marking center
+
+d=sdxf.Drawing()
+
+d.layers.append(sdxf.Layer(name="PARABOLA"))
+
+def parabolaPoint(x,focus):
+    return (x,(x**2)/(4*focus))
+
+def addParabola(drawing,focus=1,range=(-5.0,5.0),delta=0.1):
+    middle = sum(range)/2.0
+    if (middle - range[0] < delta):
+        p1 = parabolaPoint(range[0],focus)
+        p2 = parabolaPoint(range[1],focus)
+        drawing.append(sdxf.Line(points=[p1,p2],layer="PARABOLA"))
+    else:
+        addParabola(drawing,focus,(range[0],middle),delta)
+        addParabola(drawing,focus,(middle,range[1]),delta)
+
+
+
+d.append(sdxf.Circle(center=(0,focus),radius=centerRadius,layer="PARABOLA"))
+addParabola(d,focus,range,delta)
+
+
+d.saveas("parabola.dxf")
+
+    
+    
+    
