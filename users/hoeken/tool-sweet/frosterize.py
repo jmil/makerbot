516100c1f5e59902ddda7c40f37f209d8264be11
hoeken <hoeken@89f5bb60-11dc-4128-b5cc-92810caebcc6>
2010-04-27 21:17:40 +0000

    added early shutdown and z height
    git-svn-id: http://svn.makerbot.com@914 89f5bb60-11dc-4128-b5cc-92810caebcc6

--- users/hoeken/tool-sweet/frosterize.py
+++ users/hoeken/tool-sweet/frosterize.py
@@ -9,6 +9,7 @@ Usage: python frosterizer.py [options] file > output.gcode
 Options:
   -h, --help						show this help
   --z-feedrate						the Z axis feedrate in mm/min.  default 150
+  --z-height						the Z axis print height in mm.  default 0.25
   --xy-feedrate						the XY axes feedrate in mm/min. default 3500
   --width						the maximum width of the build surface in mm. default 80.
   --height						the maximum height of the build surface in mm.  default 80.
@@ -16,13 +17,9 @@ Options:
   --stop-delay						the delay after the relief valve opens before movement in milliseconds.  default 150
   --dot-delay						the delay after pressure valve opens for printing a single dot in milliseconds.  default: 50
   --line-width						the width of the line the Frostruder can draw in mm.  default 0.50
-  --border						draw a border around your drawing.  default: off 
+  --border						draw a border around your drawing.  default: off
+  --stop-distance					the distance to shut down the extruder before the end of a line in mm.  default 1.0
 """
-
-#TODO
-#
-# implement z height
-# implement early shutoff
 
 from math import *
 import sys
@@ -33,7 +30,7 @@ from PIL import ImageEnhance
 
 class Frosterizer:
 	"Class to handle generating frosting code."
-	def __init__(self, img_path, z, xy, width, height, start_delay, stop_delay, dot_delay, line_width, border):
+	def __init__(self, img_path, z_feedrate, z_height, xy_feedrate, width, height, start_delay, stop_delay, dot_delay, line_width, border, stop_distance):
 
 		self.file = img_path
 		self.current_x = 0
@@ -42,8 +39,9 @@ class Frosterizer:
 		
 		self.safe_height = 0
 
-		self.z_feedrate = z
-		self.xy_feedrate = xy
+		self.z_feedrate = z_feedrate
+		self.z_height = z_height
+		self.xy_feedrate = xy_feedrate
 		self.width = width
 		self.height = height
 		self.start_delay = start_delay
@@ -51,6 +49,7 @@ class Frosterizer:
 		self.dot_delay = dot_delay
 		self.line_width = line_width
 		self.border = border
+		self.stop_distance = stop_distance
 	
 		self.load_image()
 
@@ -100,34 +99,35 @@ class Frosterizer:
 		print "G21 (metric ftw)"
 		print "G90 (absolute mode)"
 		print "G92 X0 Y0 Z0 (zero all axes)"
-		self.go_to_point(0, 0, 0, self.z_feedrate)
+		self.go_to_point(0, 0, self.z_height, self.z_feedrate)
 		print
 	
 		if self.border:
 			min_x = self.x_pixel_to_point(-1)
 			max_x = self.x_pixel_to_point(self.image.size[0])
-			min_y = self.y_pixel_to_point(-1)
-			max_y = self.y_pixel_to_point(self.image.size[1])
+			min_y = self.y_pixel_to_point(self.image.size[1])
+			max_y = self.y_pixel_to_point(-1)
 
 			print "(border outline)"
-			self.go_to_point(min_x, max_y, 0, self.xy_feedrate)
-			print("M106 (pressure on)")
-			print("G4 P%d (wait %dms)") % (self.start_delay, self.start_delay)
-			self.go_to_point(max_x, max_y, 0, self.xy_feedrate)
-			self.go_to_point(max_x, min_y, 0, self.xy_feedrate)
-			self.go_to_point(min_x, min_y, 0, self.xy_feedrate)
-			self.go_to_point(min_x, max_y, 0, self.xy_feedrate)
-			print("M107 (pressure off)");
-			print("M126 (relief valve open)")
-			print("G4 P%d (wait %dms)") % (self.stop_delay, self.stop_delay)
-			print("M127 (relief valve close)")
+			self.go_to_point(min_x, max_y, self.z_height, self.xy_feedrate)
+			print "M106 (pressure on)"
+			print "G4 P%d (wait %dms)" % (self.start_delay, self.start_delay)
+			self.go_to_point(max_x, max_y, self.z_height, self.xy_feedrate)
+			self.go_to_point(max_x, min_y, self.z_height, self.xy_feedrate)
+			self.go_to_point(min_x, min_y, self.z_height, self.xy_feedrate)
+			self.go_to_point(min_x, max_y - self.stop_distance, self.z_height, self.xy_feedrate)
+			print "M107 (pressure off)"
+			print "M126 (relief valve open)"
+			self.go_to_point(min_x, max_y, self.z_height, self.xy_feedrate)
+			print "G4 P%d (wait %dms)" % (self.stop_delay, self.stop_delay)
+			print "M127 (relief valve close)"
 			print
 		
 		for y in range(self.image.size[1]):
 			x = 0
 			actual_y = self.y_pixel_to_point(y)
 			
-			self.go_to_point(self.current_x, actual_y, 0, self.xy_feedrate)
+			self.go_to_point(self.current_x, actual_y, self.z_height, self.xy_feedrate)
 			print
 
 			while x < self.image.size[0]:
@@ -142,28 +142,36 @@ class Frosterizer:
 						actual_end = self.x_pixel_to_point(end)
 
 						print "(line from %.2f to %.2f at %.2f)" % (actual_start, actual_end, actual_y)
-						self.go_to_point(actual_start, actual_y, 0, self.xy_feedrate)
-						print("M106 (pressure on)")
-						print("G4 P%d (wait %dms)") % (self.start_delay, self.start_delay)
-						self.go_to_point(actual_end, actual_y, 0, self.xy_feedrate)
-						print("M107 (pressure off)");
-						print("M126 (relief valve open)")
-						print("G4 P%d (wait %dms)") % (self.stop_delay, self.stop_delay)
-						print("M127 (relief valve close)")
+						self.go_to_point(actual_start, actual_y, self.z_height, self.xy_feedrate)
+						print "M106 (pressure on)"
+						print "G4 P%d (wait %dms)" % (self.start_delay, self.start_delay)
+						
+						if (abs(actual_end - actual_start) > self.stop_distance):
+							self.go_to_point(actual_end-self.stop_distance, actual_y, self.z_height, self.xy_feedrate)
+							print "M107 (pressure off)"
+							print "M126 (relief valve open)"
+							self.go_to_point(actual_end, actual_y, self.z_height, self.xy_feedrate)
+						else:
+							self.go_to_point(actual_end, actual_y, self.z_height, self.xy_feedrate)
+							print "M107 (pressure off)"
+							print "M126 (relief valve open)"
+
+						print "G4 P%d (wait %dms)" % (self.stop_delay, self.stop_delay)
+						print "M127 (relief valve close)"
 						print
 
 						x = end
 					else:
 						actual_x = self.x_pixel_to_point(x)
 						
-						print ("(dot at %.2f, %.2f)") % (actual_x, actual_y)
-						self.go_to_point(actual_x, actual_y, 0, self.xy_feedrate)
-						print("M106 (pressure on)")
-						print("G4 P%d (wait %dms)") % (self.dot_delay, self.dot_delay)
-						print("M107 (pressure off)");
-						print("M126 (relief valve open)")
-						print("G4 P%d (wait %dms)") % (self.stop_delay, self.stop_delay)
-						print("M127 (relief valve close)")
+						print "(dot at %.2f, %.2f)" % (actual_x, actual_y)
+						self.go_to_point(actual_x, actual_y, self.z_height, self.xy_feedrate)
+						print "M106 (pressure on)"
+						print "G4 P%d (wait %dms)" % (self.dot_delay, self.dot_delay)
+						print "M107 (pressure off)"
+						print "M126 (relief valve open)"
+						print "G4 P%d (wait %dms)" % (self.stop_delay, self.stop_delay)
+						print "M127 (relief valve close)"
 						print
 				x = x+1
 
@@ -171,8 +179,8 @@ class Frosterizer:
 		print "(end of print job)"
 		print "M107"
 		print "M126"
-		self.go_to_point(self.current_x, self.current_y, 10, self.xy_feedrate)
-		self.go_to_point(0, 0, 10, self.xy_feedrate)
+		self.go_to_point(self.current_x, self.current_y, 15, self.xy_feedrate)
+		self.go_to_point(0, 0, 15, self.xy_feedrate)
 		print "M18 (drives off)"
 		print "M127"
 		
@@ -195,14 +203,17 @@ def main(argv):
 			"line-width=",
 			"start-delay=",
 			"stop-delay=",
+			"stop-distance=",
 			"width=",
 			"xy-feedrate=",
 			"z-feedrate=",
+			"z-height="
 		])
 	except getopt.GetoptError:
 		usage()
 		sys.exit(2)
         
+	z_height = 0.25
 	z_feedrate = 150
 	xy_feedrate = 3500
 	width = 80
@@ -212,6 +223,7 @@ def main(argv):
 	dot_delay = 50
 	line_width = 0.50
 	border = False
+	stop_distance = 1.0
 
 	for opt, arg in opts:
 		if opt in ("-h", "--help"):
@@ -219,6 +231,8 @@ def main(argv):
 			sys.exit()
 		elif opt in ("--z-feedrate"):
 			z_feedrate = float(arg)
+		elif opt in ("--z-height"):
+			z_height = float(arg)
 		elif opt in ("-xy-feedrate"):
 			xy_feedrate = float(arg)
 		elif opt in ("--width"):
@@ -235,8 +249,10 @@ def main(argv):
 			line_width = float(arg)
 		elif opt in ("--border"):
 			border = True
+		elif opt in ("--stop-distance"):
+			stop_distance = float(arg)
 		
-	frosty = Frosterizer(argv[-1], z_feedrate, xy_feedrate, width, height, start_delay, stop_delay, dot_delay, line_width, border)
+	frosty = Frosterizer(argv[-1], z_feedrate, z_height, xy_feedrate, width, height, start_delay, stop_delay, dot_delay, line_width, border, stop_distance)
 	frosty.generate()
 
 def usage():
