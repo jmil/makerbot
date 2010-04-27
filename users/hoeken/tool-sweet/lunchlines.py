50e8a5feb9e7d0106c6f2e72123519995a672d70
phooky <phooky@89f5bb60-11dc-4128-b5cc-92810caebcc6>
2010-04-27 22:15:38 +0000

    lunchlines script in top level dir
    git-svn-id: http://svn.makerbot.com@917 89f5bb60-11dc-4128-b5cc-92810caebcc6

--- users/hoeken/tool-sweet/lunchlines.py
+++ users/hoeken/tool-sweet/lunchlines.py
@@ -1,1 +1,20 @@
-# Coming soon.  DXF and SVG to gcode converter.
+from lunchline.import_dxf import DxfParser
+from lunchline.context import GCodeContext
+from sys import stdin
+
+def main():
+    parser = DxfParser(stdin)
+    context = GCodeContext()
+    parser.parse()
+    for entity in parser.entities:
+        entity.get_gcode(context)
+    for line in context.codes:
+        print line
+
+if __name__ == "__main__":
+    main()
+else:
+    raise RuntimeError("Lunchline the top-level script, and not meant to be imported.")
+
+
+
