863183b2c2775d9f2be853e5f5d0979d586d322b
hoeken <hoeken@89f5bb60-11dc-4128-b5cc-92810caebcc6>
2010-06-24 22:11:22 +0000

    made a few fixes to the stages.
    git-svn-id: http://svn.makerbot.com@1062 89f5bb60-11dc-4128-b5cc-92810caebcc6

--- users/hoeken/test-scripts/axis-tester.php
+++ users/hoeken/test-scripts/axis-tester.php
@@ -7,21 +7,23 @@
 	$minutes = $printLength * 60 * 24;
 	$totalLength = $minutes * $feedrate;
 	$moveLength = $totalLength / $operations;
+	$axisLength = round(sqrt(($moveLength*$moveLength)/2), 3);
 	
 	//output a bit of info.
 	echo "(Axis Tester Script)\n";
 	echo "(Runtime: {$printLength} days)\n";
 	echo "(Operations: {$operations})\n";
 	echo "(Move length: {$moveLength})\n";
+	echo "(Axis length: {$axisLength})\n";
 	echo "\n";
 	echo "G92 X0 Y0 Z0 (set to home)\n";
 	echo "G90 (absolute positioning)\n";
 	
 	//actually output the moves.
-	$target = $moveLength/2;
+	$target = $axisLength/2;
 	for ($i=0; $i<$operations; $i++)
 	{
-		echo "G1 X{$target} F{$feedrate} ({$i})\n";
+		echo "G1 X{$target} Y{$target} F{$feedrate} ({$i})\n";
 		$target = -$target;
 	}
 	
